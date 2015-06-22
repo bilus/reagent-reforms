590f71e6387225ebba4cf864ec7d758b6ede5c8d
Martin Bilski <martinb@criticue.com>
2015-06-22 16:34:21 +0200

    Insert back dependency on reforms core library.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                            [org.clojure/clojurescript "0.0-3308"]
-                           ;[reforms "0.3.0-SNAPSHOT"]
+                           [reforms "0.3.0-SNAPSHOT"]
                            [reagent "0.5.0"]]
             :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
             :auto-clean false
@@ -24,31 +24,31 @@
             :aliases {"cljsbuild" ["with-profile" "dev" "cljsbuild"]}
 
             :cljsbuild {:builds {:hello-world
-                                 {:source-paths ["examples/shared/" "examples/hello_world/src" "src" "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/hello_world/src" "src" #_"../reforms/src"]
                                   :compiler     {:output-to     "examples/hello_world/out/main.js"
                                                  :output-dir    "examples/hello_world/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :controls
-                                 {:source-paths ["examples/shared/" "examples/controls/src" "src" "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/controls/src" "src" #_"../reforms/src"]
                                   :compiler     {:output-to     "examples/controls/out/main.js"
                                                  :output-dir    "examples/controls/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :simple
-                                 {:source-paths ["examples/shared/" "examples/simple/src" "src" "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/simple/src" "src" #_"../reforms/src"]
                                   :compiler     {:output-to     "examples/simple/out/main.js"
                                                  :output-dir    "examples/simple/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :progress
-                                 {:source-paths ["examples/shared/" "examples/progress/src" "src" "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/progress/src" "src" #_"../reforms/src"]
                                   :compiler     {:output-to     "examples/progress/out/main.js"
                                                  :output-dir    "examples/progress/out"
                                                  :source-map    true
                                                  :optimizations :none}}
                                  :validation
-                                 {:source-paths ["examples/shared/" "examples/validation/src" "src" "../reforms/src"]
+                                 {:source-paths ["examples/shared/" "examples/validation/src" "src" #_"../reforms/src"]
                                   :compiler     {:output-to     "examples/validation/out/main.js"
                                                  :output-dir    "examples/validation/out"
                                                  :source-map    true

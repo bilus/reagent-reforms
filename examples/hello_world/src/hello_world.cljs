5076331f851b59b4fb6c6ea8f2f3c7cd4e4acaa5
Martin Bilski <martinb@criticue.com>
2015-06-22 21:33:06 +0200

    Reformat source.

--- examples/hello_world/src/hello_world.cljs
+++ examples/hello_world/src/hello_world.cljs
@@ -4,28 +4,29 @@
             [examples.shared.utils :refer [inspector-view]]))
 
 (defn simple-view
-      [data]
-      (let [horizontal-orientation (atom false)]
-           (fn []
-               (f/with-options {:form {:horizontal @horizontal-orientation}}
-                               (f/panel
-                                 "Hello, world"
-                                 ;[inspector-view horizontal-orientation]
-                                 (f/form
-                                   {:on-submit #(js/alert "Submitted")}
-                                   (f/text "Your name" "Type your name here" data [:name])
-                                   (f/form-buttons
-                                     (f/button-primary "Submit" #(js/alert (:name @data)))
-                                     (f/button-default "Cancel" #(js/alert "Cancel!")))
-                                   (f/checkbox "Horizontal form" horizontal-orientation [])))))))
+  [data]
+  (let [horizontal-orientation (atom false)]
+    (fn []
+      (f/with-options
+        {:form {:horizontal @horizontal-orientation}}
+        (f/panel
+          "Hello, world"
+          ;[inspector-view horizontal-orientation]
+          (f/form
+            {:on-submit #(js/alert "Submitted")}
+            (f/text "Your name" "Type your name here" data [:name])
+            (f/form-buttons
+              (f/button-primary "Submit" #(js/alert (:name @data)))
+              (f/button-default "Cancel" #(js/alert "Cancel!")))
+            (f/checkbox "Horizontal form" horizontal-orientation [])))))))
 
 (def app-state (atom {:name "John"}))
 
 (defn main-view
-      []
-      [:div
-       [simple-view app-state]
-       [inspector-view app-state]])
+  []
+  [:div
+   [simple-view app-state]
+   [inspector-view app-state]])
 
 (render-component
   [main-view]
